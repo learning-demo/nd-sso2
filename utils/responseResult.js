be6646d66948455a097000b5d228880b77b95151
pengma <matthew.peng@cargosmart.com>
2021-07-03 03:53:24 +0800

    !update global response result

--- utils/responseResult.js
+++ utils/responseResult.js
@@ -1,20 +1,15 @@
 module.exports = function (req, res, next) {
-  res.sendResult = function (data, code, message) {
+  res.sendResult = function (data, code, message, statusCode) {
     var fmt = req.query.fmt ? req.query.fmt : 'rest';
     if (fmt == 'rest') {
-      if (code) {
-        res.status(code).json({
-          status: code,
-          msg: message,
-          data: data,
-        });
-      } else {
-        res.json({
-          status: code,
-          msg: message,
-          data: data,
-        });
-      }
+
+      code = code ? code : 200
+      statusCode = statusCode ? statusCode : code
+      res.status(statusCode).json({
+        status: code,
+        msg: message,
+        data: data,
+      });
 
     }
   };
